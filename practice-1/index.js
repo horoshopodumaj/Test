c95a0a9882598361944172c385a95d3b45d23bd5
Diana Murtazina <murka8831@yandex.ru>
2022-09-20 22:00:31 +0400

    closing a modal window by clicking in a free area

--- practice-1/index.js
+++ practice-1/index.js
@@ -2,6 +2,7 @@
     const close = document.getElementById("modal-close");
     const modal = document.getElementById("modal");
     const modalOpen = document.getElementById("modal-open");
+    const body = document.querySelector("body");
 
     if (!close || !modalOpen || !modal) {
         return;
@@ -12,6 +13,12 @@
         document.body.style.overflow = "hidden";
         document.body.addEventListener("keydown", (event) => {
             if (event.code === "Escape") {
+                modal.style.display = "none";
+                document.body.style.overflow = "initial";
+            }
+        });
+        modal.addEventListener("click", (event) => {
+            if (event.target.className === "modal") {
                 modal.style.display = "none";
                 document.body.style.overflow = "initial";
             }
